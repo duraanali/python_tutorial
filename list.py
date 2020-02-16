95450027a4e8e6501c5aea5afaf95fac8dba06e9
Duraan Ali <ali@sahalsolutions.com>
2020-02-16 09:42:25 -0600

    List

--- list.py
+++ list.py
@@ -1,1 +1,101 @@
-# List
+# List - collection, ordered, changeable, duplicate
+
+# Waxaa ku dari kartaa data types
+
+# fruit = ["Mango", "Apple", "Grape"]
+
+# Index = number lagu garto oo ka bilowdo ZERO
+
+# print(fruit[2])
+
+# Slicing = Jar jarid
+
+# fruit = ["Mango", "Apple", "Grape", "Banana", "Kiwi", "Melon", "Blackberry"]
+
+# print(fruit[-4:])
+
+# Change item in list
+
+# fruit = ["Mango", "Banana", "Kiwi", "Melon", "Blackberry"]
+
+# fruit[1] = "Apple"
+
+# print(fruit)
+
+# Loop Through list
+# fruit = ["Mango", "Banana", "Kiwi", "Melon", "Blackberry"]
+
+# for index, x in enumerate(fruit):
+#     print(index, x)
+
+# Check if Item exists
+# fruit = ["Mango", "Banana", "Kiwi", "Melon", "Blackberry"]
+
+# if "Apple" in fruit:
+#     print("Yes, Banana is in list")
+# else:
+#     print("No!")
+
+# print(len(fruit))
+
+# How to add item in List
+
+# fruit = ["Mango", "Banana", "Kiwi", "Melon", "Blackberry"]
+
+# fruit.append("Apple")
+
+# fruit.insert(0, "apple")
+
+# fruit2 = ["Apple", "Blueberry"]
+
+# fruit.extend(fruit2)
+
+# print(fruit + fruit2)
+
+# How to remove from list
+
+# fruit = ["Mango", "Banana", "Kiwi", "Melon", "Blackberry"]
+
+# fruit.remove("Mango")
+# removedItem = fruit.pop()
+
+# print(removedItem)
+# print(fruit)
+
+# Reverse List
+
+# fruit = ["Mango", "Banana", "Kiwi", "Melon", "Blackberry"]
+
+# fruit.sort(reverse=True)
+# fruit.reverse()
+
+# print(fruit)
+
+# Sorted Items
+# fruit = ["Mango", "Banana", "Kiwi", "Melon", "Blackberry"]
+# nums = [1, 5, 3, 2, 4, 6]
+
+# nums.sort()
+
+# sortedNumber = sorted(nums)
+
+# print(sortedNumber)
+# print(nums)
+
+# Max, Min, Sum
+
+# nums = [1, 5, 3, 2, 4, 6]
+
+# print(sum(nums))
+
+# Make list a string
+
+# fruit = ["Mango", "Banana", "Kiwi", "Melon", "Blackberry"]
+
+# fruit_str = " , ".join(fruit)
+# fruit_list = fruit_str.split(" , ")
+
+# fruit_list = list(fruit_str)
+
+
+# print(fruit_list)
