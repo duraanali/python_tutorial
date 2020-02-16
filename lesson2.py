27338676cfb62de7679e64d6204bd3065b7a2fba
Duraan Ali <ali@sahalsolutions.com>
2020-02-16 07:56:26 -0600

    Hello world and first few lessons

---
+++ lesson2.py
@@ -0,0 +1,169 @@
+# 1. Comment
+
+# """
+# This is my first code
+# this is my first code
+# """
+# x = 5
+# print(x)
+
+# 2. Variable
+
+# No declare
+
+# x = 10
+# y = "abdi"
+
+# Rules for variable:
+# 1. Must start with letter or underscore
+# 2. can not start with hashtag #
+# 3. Alpha Numeric = A-Z, 0-9, or _
+
+# x, y = 10, "abdi"
+
+# print(x, y)
+
+# x = "my name is "
+# y = 8
+
+# print(x + y)
+
+# Global Variable = haduusan variable-ka function ku jirin
+
+# 3. Data Types:
+
+# 1. String = xarfo, example
+
+# x = "Duraan"
+
+# y = 10
+
+# myString = str(y)
+# print(type(myString))
+
+# 2. Integar
+
+# x = 20
+# y = -20
+
+# myNum = 2.5
+# myNumInt = int(myNum)
+# print(type(myNumInt))
+
+# 3. Float
+
+# x = 4.5
+# y = 10
+# flt = float(y)
+# print(flt)
+
+# 4. list
+
+# x = ["moos", "canbo", "qaro"]
+
+# myItems = (("hilib", "bariis", "sonkor"))
+
+# myList = list(myItems)
+# print(x)
+
+# 5. Tuple
+
+# x = ("hilib", "bariis", "sonkor")
+
+# items = (("hilib", "bariis", "sonkor"))
+
+# myTyple = tuple(items)
+
+# 6. Dictionary
+
+# x = {"name": "abdi", "age": 30}
+
+# y = dict(name="abdi", age=30)
+
+# print(type(y))
+
+# 7. Set
+
+# x = {"canab", "canbo", "moos"}
+
+# mySet = set()
+
+# 8. Bool
+
+# True or False
+
+# 4. Casting - Badal data type
+
+# 5. Operators
+
+# Arithmetic
+# +, -, *, /
+# Modulus - Soo haraa
+
+# x = 13
+# y = 3
+
+# print(x % y)
+
+# Exponentiation - Saar
+
+# x = 5
+# y = 3
+
+# print(x ** y)
+
+# Floor
+
+# x = 17
+# y = 2
+
+# print(x // y)
+
+
+# Comparison - Isku bar bar dhig
+
+# Isku mid: ==
+# Isku mid ma aha:  !=
+# Ka badan: >
+# Ka yar: <
+# La eg ama ka wayn: >=
+# Ka yar ama la eg: <=
+
+# x = 10
+# y = 10
+
+# print(x <= y)
+
+
+# Logical Operators
+
+# and, or, not
+
+# y = 5
+# x = 5
+# t = 10
+# j = 8
+
+# if y == x and t == j:
+#     print('They are all same')
+# else:
+#     print('not all same')
+
+# Identity Operators - true or false
+
+# is, is not
+
+# x = 5
+# y = 5
+
+# print(x is not y)
+
+# Membership - true or false
+
+# in, not in
+
+# x = ["canbo", "qaro"]
+
+# y = "moos"
+
+# print(y not in x)
