9f3d217dc77c766c60c20c7afa2278ba27f56d5d
Duraan Ali <duraan522@gmail.com>
2020-02-29 19:37:53 -0600

    Tuple

---
+++ tuple.py
@@ -0,0 +1,53 @@
+# fruit = ("Mango", "Apple", "Banana", "Grape", "Blackberry")
+
+# x = list(fruit)
+# x[0] = "Canbo"
+
+# y = tuple(x)
+# print(y)
+
+# for x in fruit:
+#     print(x)
+
+# fruit = ("Mango", "Apple", "Banana", "Grape", "Blackberry")
+
+# for index, x in enumerate(fruit):
+#     print(index, x)
+
+# fruit = ("Mango", "Apple", "Banana", "Grape", "Blackberry")
+
+# if "Apple" in fruit:
+#     print("Yes")
+# else:
+#     print("No")
+
+# HUBI INAAD COMMA KU DARTO
+
+# one_fruit = ('Apple',)
+
+# print(type(one_fruit))
+
+# How to delete
+
+# fruit = ("Mango", "Apple", "Banana", "Grape", "Blackberry")
+
+# del fruit
+
+# print(fruit)
+
+# How to add 2 tuples
+
+# num1 = (1, 3, 4)
+# num2 = (5, 3, 7)
+
+# nums = num1 + num2
+
+# print(nums)
+
+# Unpacking sida loo sameeyo
+
+# student = ("Duraan", "Math", 30)
+
+# (name, subject, age) = student
+
+# print(name, subject, age)
