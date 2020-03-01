4fd20158422a8d3b5683219d2ea599ed787ac413
Duraan Ali <duraan522@gmail.com>
2020-02-29 22:04:24 -0600

    sets

---
+++ sets.py
@@ -0,0 +1,44 @@
+# Sets = Unorderd, Unindexed, Unique, uneditable
+
+# fruit = {"Apple", "Banana", "Mango", "Blackberry"}
+
+# Accessing
+
+# for x in fruit:
+#     print(x)
+
+# Add to Set
+
+# fruit = {"Apple", "Banana", "Mango", "Blackberry"}
+
+# fruit.add("Orange")
+# print(fruit)
+
+# Add multiple items
+# fruit = {"Apple", "Banana", "Mango", "Blackberry"}
+
+# fruit.update(["Grapes", "Orange", "Strawberry"])
+
+# print(fruit)
+
+#Remove Item
+
+# fruit = {"Apple", "Banana", "Mango", "Blackberry"}
+
+# fruit.remove("Banana")
+# fruit.discard("orange")
+
+# fruit.pop()
+
+# fruit.clear()
+
+# print(fruit)
+
+# How to combine two sets
+# fruit = {"Apple", "Banana", "Mango", "Blackberry"}
+# fruit2 = {"Orange", "Banana"}
+
+# # fruit3 = fruit.union(fruit2)
+# fruit.update(fruit2)
+
+# print(fruit)
